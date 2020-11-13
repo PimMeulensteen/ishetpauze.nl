76fff07b18a3c82afa3fa576a2bd18b26561075d
Pim Meulensteen <pim@meulensteen.net>
2020-11-13 11:51:23 +0100

    added confetti

--- pauze.js
+++ pauze.js
@@ -1,17 +1,20 @@
-function isPauze () {
-  const d = new Date()
-  const t = d.getMinutes()
+function isPauze() {
+    const d = new Date()
+    const t = d.getMinutes()
 
-  if (t < 45) {
-    if (t == 1){
-    elem.innerText = `Nee, nog ${45 - t} minuut`
-    } else{
-      elem.innerText = `Nee, nog ${45 - t} minuten`
+    if (t < 45) {
+        if (t == 1) {
+            elem.innerText = `Nee, nog ${45 - t} minuut`
+        } else {
+            elem.innerText = `Nee, nog ${45 - t} minuten`
+        }
+        confetti.stop();
+    } else {
+        elem.innerText = 'Ja, het is pauze!'
+        confetti.start();
     }
-  } else {
-    elem.innerText = 'Ja, het is pauze!'
-  }
 }
+let conf_playing = false;
 const elem = document.getElementById('result')
 isPauze()
 setInterval(isPauze, 1000)
